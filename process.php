27bc3bb5da393a90262c23018f82d1451f1630db
Meenaxijb <meenaxi.badola@justwords.inm>
2023-08-06 21:11:58 +0530

    updated

--- process.php
+++ process.php
@@ -1,17 +1,36 @@
-<?php
- // Get Data 
- $name = strip_tags($_POST['name']);
- $email = strip_tags($_POST['email']);
- $phone = strip_tags($_POST['phone']);
- $url = strip_tags($_POST['url']);
- $message = strip_tags($_POST['message']);
+<?php 
+if(isset($_POST['submit'])){
+    $to = "email@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
- $headers .="From: Forms <forms@example.net>";
- $headers .="CC: Mail1 <Mail1@example.net>";
- $headers .=", Mail2 <Mail2@example.net>";
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
 
- // Send Message
- mail( "meenaxibadola1@gmail.com.com", "Contact Form Submission",
- "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $url\nMessage: $message\n",
-  $headers );
- ?>
+<!DOCTYPE html>
+<head>
+<title>Form submission</title>
+</head>
+<body>
+
+<form action="" method="post">
+First Name: <input type="text" name="first_name"><br>
+Last Name: <input type="text" name="last_name"><br>
+Email: <input type="text" name="email"><br>
+Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html> 
