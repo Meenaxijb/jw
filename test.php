14849ca91add0f1117436718493e8c3ee2307f98
Meenaxijb <meenaxi.badola@justwords.inm>
2023-08-07 13:48:14 +0530

    update

--- test.php
+++ test.php
@@ -1,3 +1,120 @@
-<?php
-phpinfo();
-?>
+<form class="row g-3" method="post" action="">
+    <h5>Marketing Business Campaign</h5>
+    <h2>Get a Quote</h2>
+   <div class="col-md-12">
+    <div class="input-group input-group-lg">
+     <input type="text" class="form-control" placeholder="Complete Name" id="firstname" name="flname" required>
+ <div class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+ </svg>
+ </div>
+ </div>    
+ </div>
+ 
+     <div class="col-md-12">
+     <div class="input-group input-group-lg">
+         <input type="email" class="form-control" placeholder="Email Address" id="firstname" name="email" required>
+ <div class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
+   <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z"/>
+   <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
+ </svg></div>
+     </div>
+   </div>
+ 
+  
+ <div class="col-md-12">
+ <div class="input-group input-group-lg">
+ <input type="tel" class="form-control" placeholder="Cell" id="cell" name="telphone" required>
+ <div class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-inbound" viewBox="0 0 16 16">
+  <path d="M15.854.146a.5.5 0 0 1 0 .708L11.707 5H14.5a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 1 0v2.793L15.146.146a.5.5 0 0 1 .708 0zm-12.2 1.182a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+ </svg>
+ </div>
+     </div>
+   </div>
+  
+  
+  
+   <div class="col-md-12">
+ <div class="input-group input-group-lg">
+ <select class="form-select" aria-label="Default select example" name="selectoption" required>
+   <option selected>Sevices</option>
+   <option value="Physician Billing">Physician Billing</option>
+   <option value="Hospital Billing">Hospital Billing</option>
+   <option value="LAB Billing">LAB Billing</option>
+   <option value="DME Billing">DME Billing</option>
+  
+    <option value="Accounts Receivables">Accounts Receivables</option>
+     <option value="Charge Entry">Charge Entry</option>
+ <option value="Payment posting">Payment posting</option>
+  <option value="Credentialing">Credentialing</option>
+   <option value="Transcriptions">Transcriptions</option>
+  
+  
+  
+ </select>
+ 
+ <div class="input-group-text">
+ <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-prescription2" viewBox="0 0 16 16">
+   <path d="M7 6h2v2h2v2H9v2H7v-2H5V8h2V6Z"/>
+   <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v10.5a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 14.5V4a1 1 0 0 1-1-1V1Zm2 3v10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V4H4ZM3 3h10V1H3v2Z"/>
+ </svg>
+ 
+ </div>
+     </div>
+   </div>
+  
+  
+     <div class="col-md-12">
+ <div class="input-group input-group-lg">
+ <textarea class="form-control" placeholder="Leave a comment here" name="message" id="floatingTextarea2" style="height: 100px"></textarea>
+ <div class="input-group-text">
+ <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-text-fill" viewBox="0 0 16 16">
+   <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
+ </svg>
+ 
+ </div>
+     </div>
+   </div>
+  
+  
+  
+    
+  
+  
+  
+   <div class="col-12">
+   <div class="d-grid gap-2">
+   <button type="submit" name="submit" class="btn btn-dark mt-3 btn-lg bg-black">Book a Free Consul</button>
+ </div>
+ </div>
+ </form>
+ 
+ 
+ <?php
+ if(isset($_POST["submit"])){
+ $name = $_POST['flname'];
+ $email = $_POST['email'];
+ $telephone= $_POST['telphone'];
+ $selectoption = $_POST['selectoption'];
+ $textarea = $_POST['message'];
+ 
+ $to = "hello@virtualoplossing.us";
+ $subject = "New form is submitted on Virtual Oplossing Healthcare";
+ $txt =" Name = ". $name . "<br> Email = ". $email . "<br> Telephone =" . $telephone . "<br> Speciality = ". $selectoption . "<br> Message =" . $textarea;
+ 
+ $headers = "MIME-Version: 1.0" . "\r\n";
+ $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+ $headers .= "From: $email" . "\r\n" ;
+ 
+ mail($to,$subject,$txt,$headers);
+ 
+ 
+ //header("Location:thank-you.php");
+ 
+  echo '<div class="alert alert-success alert-dismissible fade show" role="alert"><strong>Thanks for being awesome! <br>We have received your message and would like to thank you for writing to us.</strong><br> <br> Talk to you soon,<br>VO Healthcare
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+       </div>';
+ // exit();
+ 
+ }
+ ?>
